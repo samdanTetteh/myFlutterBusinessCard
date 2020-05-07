55756505fffc20f8fec17f85284ab87a9e1f9cda
Samdan <skudjodj@ldn-c17znwramd6n.home>
2020-05-07 11:50:49 +0100

    initial commit

---
+++ android/app/src/main/kotlin/com/ijikod/My_Business_Card/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.ijikod.My_Business_Card
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,53 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.teal,
+        body: SafeArea(
+            child:Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                CircleAvatar(
+                  radius: 50.0,
+                  backgroundColor: Colors.blue,
+                  backgroundImage: AssetImage("images/sam.jpg"),
+                ),
+                Text("Samdan Kudjodji", style: TextStyle(fontSize: 40.0, color: Colors.white, fontWeight: FontWeight.bold, fontFamily: "Pacifico")),
+                Text("MOBILE DEVELOPER", style: TextStyle(fontSize: 20.0, color: Colors.teal.shade100, fontWeight: FontWeight.bold, fontFamily: "Source Sans Pro", letterSpacing: 2.5)),
+                SizedBox(
+                  height: 20.0,
+                  width: 150.0,
+                  child: Divider(
+                    color: Colors.teal.shade100,
+                  ),
+                ),
+                Card(
+                    margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
+                    child:
+                    ListTile(
+                      leading: Icon(Icons.phone, color: Colors.teal,),
+                      title: Text("+44 725 235 62755", style: TextStyle(color: Colors.teal.shade900, fontFamily: "Source Sans Pro", fontSize: 20.0)),
+                    )
+                ),
+                Card(
+                    margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
+                    child:
+                    ListTile(
+                      leading: Icon(Icons.email, color: Colors.teal,),
+                      title:  Text("samdantettey@gmail.com", style: TextStyle(color: Colors.teal.shade900, fontFamily: "Source Sans Pro", fontSize: 20.0)),
+                    )
+                )
+              ],
+            )
+        ),
+      ),
+    );
+  }
+}
